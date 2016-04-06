50c1e73ae7940a60c1c884dd740c2a7ba4a0e2c6
Daniel Porteous <danielporteous1@gmail.com>
2016-04-06 15:19:14 +1000

    Upgraded test.sh to leave less garbage after testing. Got rid of all output text files. These can easily be generated if you want a copy. Moved some other old stuff into old_output/

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 make
-./simulation -f specInput.txt -m 100 -a multi > specInput-multi-100-out.txt
-diff specInput-multi-100-out.txt specOutput-multi-100.txt
-./simulation -f specInput.txt -m 100 -a fcfs > specInput-fcfs-100-out.txt
-diff specInput-fcfs-100-out.txt specOutput-fcfs-100.txt
-./simulation -f in_hard.txt -m 1000 -a multi > out_hard.txt
-diff out_hard.txt kurt_out.txt 
+./simulation -f specInput.txt -m 100 -a multi > out.txt
+diff out.txt specOutput-multi-100.txt
+./simulation -f specInput.txt -m 100 -a fcfs > out.txt
+diff out.txt specOutput-fcfs-100.txt
+./simulation -f in_hard.txt -m 1000 -a multi > out.txt
+diff out.txt kurt_out.txt 
 #./simulation -f exp_in.txt -m 30 -a multi
+rm out.txt
+make clean
